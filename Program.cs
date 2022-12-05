68eacfa714410817e42caf5fc2233d1abeaa0872
Nastya <anastasiya.komova@mail.ru>
2022-12-05 10:53:51 +0300

    задача 13

--- Program.cs
+++ Program.cs
@@ -17,6 +17,49 @@ else
 }
 */
 
+//Без проверки
+
+/*int Threenum(int num)
+{
+    int des = num % 100 /10;
+    return des;
+}
+Console.Write("Введите трехзначное число: ");
+int newnum = Convert.ToInt32(Console.ReadLine());
+int res = Threenum(newnum);
+Console.WriteLine(res); */
+
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//Без функции
+/*Console.Write("Введите число: ");
+int newnum = Convert.ToInt32(Console.ReadLine());
+
+if (newnum > 99)
+{
+  string str = newnum.ToString();
+    Console.WriteLine(str[2]); 
+}
+else
+{
+    Console.WriteLine("Нет третьего числа!");
+}
+*/
+
+//C функцией
+
+/* int Posnum(int num)
+{   
+    int minus = -1;
+    string str = num.ToString();
+    if (num > 99)
+        Console.WriteLine(str[2]);
+    else
+        Console.WriteLine(minus);
+    return num;
+}
+Console.Write("Введите число: ");
+int newnum = Convert.ToInt32(Console.ReadLine());
+int res = Posnum(newnum);
+*/
+
